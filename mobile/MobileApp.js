86c9fa2f583961cf1e2c45557cb6432706b21828
Sinan Gunes <gunes.sinan@gmail.com>
2017-12-26 19:45:29 +0300

    Clean

--- mobile/MobileApp.js
+++ mobile/MobileApp.js
@@ -12,10 +12,7 @@ const BoardApp = require('../board/BoardApp');
 const mobileMap = new Map();
 
 Emitters.getStateEmitter().on(Message.UPDATE_STATE, (device, state) => {
-    console.log("MobileApp_ UPDATE_STATE#1 device: " + device + " state: " + JSON.stringify(state));
-    console.log("MobileApp_ UPDATE_STATE#2 mobileMap: " + JSON.stringify(mobileMap));
     mobileMap.forEach(mobile => {
-        console.log("MobileApp_ UPDATE_STATE#3 mobile: " + JSON.stringify(mobile.user));
         if (mobile.user.device === device) {
             sendState(mobile.ws, mobile.user.name, state);
         }
@@ -42,7 +39,7 @@ function sendState(ws, username, state) {
 function orderRelay(ws, device, floor) {
     Emitters.getOrderEmitter().emit(Message.RELAY_ORDER, device, floor, err => {
         if (err) {
-            console.log('Error emitting Relay Order: ' + err)
+            console.warn(Moment().format() + ' Error emitting Relay Order: ' + err)
         }
         const message = {
             _type: Message.RELAY_ORDER_RESPONSE,
@@ -129,7 +126,6 @@ module.exports = {
         //ws.send('MobileApp, Hello: ' + username);
         mobileMap.set(user.name, {user: user, ws: ws});
         console.info(Moment().format() + ' Total Mobiles connected: ' + mobileMap.size + ' new Mobile: ' + user.name);
-        console.log("MobileApp_ onConnect mobileMap: " + JSON.stringify(mobileMap));
     },
     onMessage(user, ws, req, message) {
         switch (message._type) {
@@ -156,9 +152,7 @@ module.exports = {
         }
     },
     onClose(user, ws, req) {
-        console.log("MobileApp_ onClose1 mobileMap: " + JSON.stringify(mobileMap));
         mobileMap.delete(user.name);
-        console.log("MobileApp_ onClose2 mobileMap: " + JSON.stringify(mobileMap));
         console.info(Moment().format() + ' Total Mobiles connected: ' + mobileMap.size + ' Mobile disconnected: ' + user.name);
     },
     onAuth(user, cb) {
